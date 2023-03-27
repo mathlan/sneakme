361fd29010c6b845c99c37aebc4893f728e0ee5b
Paul <paul.philippe@viacesi.fr>
2023-03-27 14:52:57 +0200

    14522703Paul

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,99 @@ class DatabaseSeeder extends Seeder
         \App\Models\Category::factory(10)->create();
         \App\Models\Product::factory(30)->create();
 
-        // Questions de politesses
+        // Réponses
 
         \App\Models\Answer::factory()->create([
+            'id' => '1',
              'name' => 'Bonjour à vous',
          ]);
+
+        \App\Models\Answer::factory()->create([
+            'id' => '2',
+            'name' => 'Voici notre catalogue',
+        ]);
+
+        \App\Models\Answer::factory()->create([
+            'id' => '3',
+            'name' => 'Voici notre gamme de chaussures',
+        ]);
+
+        \App\Models\Answer::factory()->create([
+            'id' => '4',
+            'name' => 'Voici les marques que nous possédons',
+        ]);
+
+        \App\Models\Answer::factory()->create([
+            'id' => '5',
+            'name' => 'Votre article a été ajouté',
+        ]);
+
+        \App\Models\Answer::factory()->create([
+            'id' => '6',
+            'name' => 'Voici votre panier',
+        ]);
+
+        \App\Models\Answer::factory()->create([
+            'id' => '7',
+            'name' => 'Inscrivez-vous',
+        ]);
+
+        \App\Models\Answer::factory()->create([
+            'id' => '8',
+            'name' => 'Vous Pouvez vous connecter ici',
+        ]);
+
+        // Mot-clés
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'bonjour',
+            'answer_id' => "1",
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'catalogue',
+            'answer_id' => "2",
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'chaussures',
+            'answer_id' => "3",
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'marques',
+            'answer_id' => "4",
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'ajouter',
+            'answer_id' => "5",
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'panier',
+            'answer_id' => "6",
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'inscrire',
+            'answer_id' => "7",
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'inscription',
+            'answer_id' => "7",
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'connecter',
+            'answer_id' => "8",
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'connexion',
+            'answer_id' => "8",
+        ]);
 
 
 
