669043fe491bf99747703d9107359d68c401d49c
MathisHoziaux <mathoz76000@gmail.com>
2023-03-27 09:00:10 +0200

    create form for test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 import './bootstrap';
+
+$(function(){// BTN BOT
+    $('.chatbot-bulle').click(function() {
+        $('.chatbot-page').toggleClass('open');
+        $('.chatbot-bulle').toggleClass('up');
+        $('.message-taping-zone').toggleClass('on');
+
+    });
+    $('.chat-header .fa-xmark').click(function() {
+        $('.chatbot-page').toggleClass('open');
+        $('.chatbot-bulle').toggleClass('up');
+        $('.message-taping-zone').toggleClass('on');
+    });
+    $('.send-message').click(function() {
+        $(".send-message i").addClass('animation-send');
+        setTimeout(function () {
+            $(".send-message i").removeClass('animation-send');
+        }, 4000);
+    });
+
+});
