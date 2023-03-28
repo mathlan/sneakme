819c6afaf075a0234951b6837ab0de349d4160d1
Paul <paul.philippe@viacesi.fr>
2023-03-28 11:31:46 +0200

    11312803Paul

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -84,6 +84,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Voila les categories disponible',
         ]);
 
+        \App\Models\Answer::factory()->create([
+            'id' => '14',
+            'name' => 'Que shouaitez-vous modifier ?',
+        ]);
+
         // Mot-clés
 
         \App\Models\Keyword::factory()->create([
@@ -91,6 +96,8 @@ class DatabaseSeeder extends Seeder
             'answer_id' => "1",
             'type' => 'default',
         ]);
+
+        // Mot-clés // TYPE catalogue
 
         \App\Models\Keyword::factory()->create([
             'name' => 'catalogue',
@@ -111,6 +118,14 @@ class DatabaseSeeder extends Seeder
         ]);
 
         \App\Models\Keyword::factory()->create([
+            'name' => 'categories',
+            'answer_id' => "13",
+            'type' => 'catalogue',
+        ]);
+
+        // Mot-clés // TYPE panier
+
+        \App\Models\Keyword::factory()->create([
             'name' => 'ajouter',
             'answer_id' => "5",
             'type' => 'panier',
@@ -123,15 +138,35 @@ class DatabaseSeeder extends Seeder
         ]);
 
         \App\Models\Keyword::factory()->create([
+            'name' => 'supprimer',
+            'answer_id' => "9",
+            'type' => 'panier',
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'commander',
+            'answer_id' => "10",
+            'type' => 'panier',
+        ]);
+
+        \App\Models\Keyword::factory()->create([
+            'name' => 'modifier',
+            'answer_id' => "14",
+            'type' => 'panier',
+        ]);
+
+        // Mot-clés // TYPE compte
+
+        \App\Models\Keyword::factory()->create([
             'name' => 'inscrire',
             'answer_id' => "7",
-            'type' => 'inscrire',
+            'type' => 'compte',
         ]);
 
         \App\Models\Keyword::factory()->create([
             'name' => 'inscription',
             'answer_id' => "7",
-            'type' => 'inscrire',
+            'type' => 'compte',
         ]);
 
         \App\Models\Keyword::factory()->create([
@@ -150,24 +185,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'compte',
             'answer_id' => "8",
             'type' => 'compte',
-        ]);
-
-        \App\Models\Keyword::factory()->create([
-            'name' => 'supprimer',
-            'answer_id' => "9",
-            'type' => 'panier',
-        ]);
-
-        \App\Models\Keyword::factory()->create([
-            'name' => 'commander',
-            'answer_id' => "10",
-            'type' => 'panier',
-        ]);
-
-        \App\Models\Keyword::factory()->create([
-            'name' => 'categories',
-            'answer_id' => "13",
-            'type' => 'catalogue',
         ]);
 
 
