c73e0a5e4b646bb511a05d4d2515a6dcf571873f
MathisHoziaux <mathoz76000@gmail.com>
2023-04-24 09:12:08 +0200

    09112404mathis

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,28 @@
 import './bootstrap';
 
+/*let answer
+let textbox = document.getElementById('testapi');
+async function chatBot(){
+    try {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "keyword" : document.getElementById('usernameGet').value
+            })
+        };
+        await fetch('URL', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                answer = data
+            });
+            textbox.innerHTML = answer
+    } catch (e) {
+        console.log('Erreur :' + e);
+    }
+
+}*/
+
 $(function(){// BTN BOT
     $('.chatbot-bulle').click(function() {
         $('.chatbot-page').toggleClass('open');
@@ -19,4 +42,52 @@ $(function(){// BTN BOT
         }, 4000);
     });
 
+    $('#btnGet').click(function () {
+        getUserCountry()
+    })
+
 });
+
+/* API CHATBOT */
+
+$(document).ready(function() {
+    // Gestionnaire d'évenements du form
+    $('#chat-form').submit(function(event) {
+        event.preventDefault();
+        // Récupération de l'input du form (keyword)
+        var message = $('#chat-message').val();
+        // console.log(message);
+        $('#chat-messages').append('<div><strong>' + message + '</strong></div>')
+        // S'il y a un message -> Requête
+        if (message) {
+            // Envoi au serveur
+            $.ajax({
+                url: 'api/chat',
+                method: 'POST',
+                dataType: 'json',
+                // Il faut bien envoyer les data formatées en json {keyword: data}
+                data: { keyword: message },
+                // Display de la réponse (name)
+                success: function(data) {
+                    // Si un message a été trouvé
+                    if (Object.keys(data).length != 0) {
+                        // Pour les tests en console
+                        console.log(data);
+                        console.log(typeof message + ' ' + message);
+                        $('#chat-messages').append('<div><strong>' + data.name + '</strong></div>');
+                    } else {
+                        //Si aucun message n'a été trouvé
+                        $('#chat-messages').append('<div>Merci de reformuler votre demande.</div>');
+                    }
+                },
+                error: function(xhr, status, error) {
+                    alert('Error: ' + error);
+                }
+            });
+            // On vide l'input pour la prochaine requête
+            $('#chat-message').val('');
+        }
+    });
+});
+
+
